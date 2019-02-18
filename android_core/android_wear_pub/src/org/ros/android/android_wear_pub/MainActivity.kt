36f09fde5b30a4b1b49700b4f2784dcff60adbf9
lucrezia <lucre.grassi@gmail.com>
2019-02-18 13:17:02 +0100

    Removed useless things and fixed proximity app

---
+++ android_core/android_wear_pub/src/org/ros/android/android_wear_pub/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.ros.android.android_wear_pub
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity: WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, Proximity::class.java)
+        startActivity(intent)
+    }
+}
